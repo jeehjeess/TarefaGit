49c1d13f5b22800de83697d2bde9e7e13d7a5260
20181210031 <20181210031@pc-129392.passofundo.ifsul.local>
2019-10-18 20:32:55 -0300

    Criadas 3 classes

---
+++ src/tarefagit/Classe_3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarefagit;
+
+/**
+ *
+ * @author 20181210031
+ */
+public class Classe_3 {
+    
+}
